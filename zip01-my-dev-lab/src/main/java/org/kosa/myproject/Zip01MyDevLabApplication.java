7af0a0dcf509488e819cd4abd7cb699765160064
yey3hh <wateveryehwant@gmail.com>
2025-07-28 00:21:45 +0900

    feat: basic setting

---
+++ zip01-my-dev-lab/src/main/java/org/kosa/myproject/Zip01MyDevLabApplication.java
@@ -0,0 +1,13 @@
+package org.kosa.myproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zip01MyDevLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Zip01MyDevLabApplication.class, args);
+	}
+
+}
